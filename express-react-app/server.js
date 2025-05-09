2103e0c187e449d42f44ff93ff5b9970c9d5bbd3
Daniil Dybka <danil.dybko@gmail.com>
2025-05-10 02:12:11 +0300

    work

--- express-react-app/server.js
+++ express-react-app/server.js
@@ -1,11 +1,11 @@
-const express = require('express'); //Строка 1
-const app = express(); //Строка 2
-const port = process.env.PORT || 8000; //Строка 3
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
 
 // Сообщение о том, что сервер запущен и прослушивает указанный порт
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Строка 6
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Создание GET маршрута
-app.get('/express_backend', (req, res) => { //Строка 9
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Строка 10
-}); //Строка 11
+app.get('/express_backend', (req, res) => {
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
